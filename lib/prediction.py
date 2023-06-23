017cd704d1aed3fd0d21b4715664a7c475b05790
Anthony Surya <suryaanthony2@gmail.com>
2023-06-23 17:42:49 +0700

    change prediction return value

--- lib/prediction.py
+++ lib/prediction.py
@@ -1,18 +1,24 @@
 import numpy as np
+import pathlib
+import os
 
 def classify(input_x, input_y, input_z, model):
+    path = str(pathlib.Path().resolve()) + "\\video\\train"
+
+    classes = os.listdir(path)
+
     prediction = model.predict([input_x, input_y, input_z], verbose=0)
     prediction_result = np.argmax(prediction)
     
     prediction_prob = prediction[:, prediction_result]
     
     if prediction_result == 0:
-        return "Age-Uke", prediction_prob
+        return classes[0], prediction_prob
     elif prediction_result == 1:
-        return "Gedan-Barai", prediction_prob
+        return classes[1], prediction_prob
     elif prediction_result == 2:
-        return "Mawashi-Uchi", prediction_prob
+        return classes[2], prediction_prob
     elif prediction_result == 3:
-        return "Oi-Zuki", prediction_prob
+        return classes[3], prediction_prob
     elif prediction_result == 4:
-        return "Shuto-Uke", prediction_prob
+        return classes[4], prediction_prob
