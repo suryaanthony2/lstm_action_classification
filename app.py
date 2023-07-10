5074a4d2e4afa7e81bbb14a4188e3d1f0651f987
Anthony Surya <suryaanthony2@gmail.com>
2023-07-10 11:24:10 +0700

    Change app.py program to OOP

--- app.py
+++ app.py
@@ -15,89 +15,87 @@ from pygrabber.dshow_graph import FilterGraph
 
 import sv_ttk
 
-class app(tk.Tk):
+class App(tk.Tk):
     def __init__(self):
         super().__init__()
-
-        self.model = load_model(os.fspath(pathlib.Path(__file__).parent / "model/LSTM_model"), compile=False)
-
-        self.file_path = ""
-        self.multi_file_path = ""
-        self.res = ""
-
-        self.geometry("1000x450")
-        self.title("Karate app")
+    
+        container = tk.Frame(self)
         sv_ttk.set_theme("dark")
 
-        main_frame = Frame(self)
-        main_frame.pack()
+        self.geometry("800x600")
+        self.title("Karate app")
 
-        self.create_frame(main_frame)
+        container.pack(expand=True)
 
-    def create_frame(self, main_frame):
-        single_class = LabelFrame(main_frame, text="One Move Classification")
-        single_class.grid(row=0, column=0)
-        multi_class = LabelFrame(main_frame, text="Real Time Classification")
-        multi_class.grid(row=1, column=0)
-        self.single_cls_app_widgets(single_class)
-        self.multi_cls_app_widgets(multi_class)
+        self.frames = {}
+
+        for F in (StartPage, SingleClass, MultiClass):
+            page_name = F.__name__
+            frame = F(parent=container, controller=self)
+            
+            self.frames[page_name] = frame
+
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        self.show_frame("StartPage")
         
-        for frame in main_frame.winfo_children():
-            frame.grid_configure(padx=10, pady=20)
+    def show_frame(self, page_name):
+        frame = self.frames[page_name]
+        frame.tkraise()
 
-    def single_cls_app_widgets(self, frame):
-        self.single_cls_btn_lbl = Label(frame, text="please choose mp4 file")
-        self.single_cls_btn_lbl.grid(row=0, column=0)
-        self.single_cls_btn = Button(frame, text="Select File", width = 40, command=self.get_file_path)
-        self.single_cls_btn.grid(row=1, column=0)
-        self.file_path_lbl = Label(frame, text="")
-        self.file_path_lbl.grid(row=2, column=0)
 
-        #button untuk melakukan inferensi
-        self.btn_inf = Button(frame, text="lakukan inferensi", width = 40, command=self.inference)
-        self.btn_inf.grid(row=0, column=1)
-        self.lbl_inf = Label(frame, text="")
-        self.lbl_inf.grid(row=1, column=1)
+class StartPage(tk.Frame):
+    def __init__(self, parent, controller):
+        super().__init__(parent)
+        self.controller = controller
 
-        for widget in frame.winfo_children():
-            widget.grid_configure(padx=10, pady=5)
+        self.title_lbl = Label(self, text="KARATE CLASSIFICATION", font=("Times new roman", 30, "bold"))
+        self.title_lbl.pack(pady=20)
 
-        self.update_single_text()
+        self.single_cls_btn = Button(self, text="Single Class Classification", width = 40, command=lambda: self.controller.show_frame("SingleClass"))
+        self.single_cls_btn.pack(pady=10)
 
-    def multi_cls_app_widgets(self, frame):
-        #widget untuk milih kamera dan real time inference
-        self.src_lbl = Label(frame, text="Choose source")
-        self.src_lbl.grid(row=0, column=1)
-        self.src_combo = ttk.Combobox(frame, values=["camera", "mp4"])
-        self.src_combo.grid(row=1, column=1)
-        self.camera_lbl = Label(frame, text="Choose camera")
-        self.camera_lbl.grid(row=2, column=0)
-        graph = FilterGraph()
-        self.devices = graph.get_input_devices()
-        self.camera_combo = ttk.Combobox(frame, values=self.devices)
-        self.camera_combo.grid(row=3, column=0)
+        self.multi_cls_btn = Button(self, text="Real Time Classification", width = 40, command=lambda: self.controller.show_frame("MultiClass"))
+        self.multi_cls_btn.pack(pady=10)
 
-        self.multi_cls_btn = Button(frame, text="Please choose mp4 file", width = 40, command=lambda: self.get_file_path(single=False))
-        self.multi_cls_btn.grid(row=2, column=2)
-        self.multi_path_lbl = Label(frame, text="")
-        self.multi_path_lbl.grid(row=3, column=2)
+class SingleClass(tk.Frame):
+    def __init__(self, parent, controller):
+        super().__init__(parent)
+        self.model = load_model(os.fspath(pathlib.Path(__file__).parent / "model/LSTM_model"), compile=False)
+        self.controller = controller
+        self.file_path = ""
+        self.res = ""
 
-        self.btn_inf = Button(frame, text="inferensi real time", width = 40, command=self.inf_rt)
-        self.btn_inf.grid(row=4, column=1)
+        self.title_lbl = Label(self, text="Single Class Classification", font=("Times new roman", 30, "bold"))
+        self.title_lbl.pack(pady=20)
 
-        for widget in frame.winfo_children():
-            widget.grid_configure(padx=10, pady=5)
+        self.single_cls_btn = Button(self, text="Select File Path", width = 40, command=self.get_file_path)
+        self.single_cls_btn.pack(pady=10)
 
-        self.update_multi_text()
+        self.file_path_lbl = Label(self, text="")
+        self.file_path_lbl.pack(pady=10)
 
-    def get_file_path(self, single=True):
-        if single:
-            self.file_path = filedialog.askopenfilename(title="Select a video File", filetypes=(("mp4", "*.mp4"), ))
-        else:
-            self.multi_file_path = filedialog.askopenfilename(title="Select a video File", filetypes=(("mp4", "*.mp4"), ))
+        self.btn_inf = Button(self, text="Prediction", width = 40, command=self.inference)
+        self.btn_inf.pack(pady=10)
+        self.lbl_inf = Label(self, text="")
+        self.lbl_inf.pack(pady=10)
+
+        self.btn_back = Button(self, text="Back", width = 40, command=lambda: self.controller.show_frame("StartPage"))
+        self.btn_back.pack(pady=10)
+
+        self.update_text()
+
+    def get_file_path(self):
+        self.file_path = filedialog.askopenfilename(title="Select a video File", filetypes=(("mp4", "*.mp4"), ))
+
+    def update_text(self):
+        if self.file_path != "":
+            self.file_path_lbl.config(text="File path = " + self.file_path)
+        if self.res != "":
+            self.lbl_inf.config(text="Prediction result = " + self.res)
+        self.after(100, self.update_text)
 
     def inference(self):
-        self.withdraw()
         if self.file_path == "":
             messagebox.showinfo("Error", "Select file first")
             return
@@ -105,23 +103,50 @@ class app(tk.Tk):
         input_x, input_y, input_z = np.split(x, 3)
 
         self.res, _ = prediction.classify(input_x, input_y, input_z, self.model)
-        self.deiconify()
 
+class MultiClass(tk.Frame):
+    def __init__(self, parent, controller):
+        super().__init__(parent)
+        self.model = load_model(os.fspath(pathlib.Path(__file__).parent / "model/LSTM_model"), compile=False)
+        self.controller = controller
+        self.file_path = ""
 
-    def update_single_text(self):
+        self.title_lbl = Label(self, text="Real Time Classification", font=("Times new roman", 30, "bold"))
+        self.title_lbl.pack(pady=20)
+
+        self.src_lbl = Label(self, text="Choose source")
+        self.src_lbl.pack(pady=5)
+        self.src_combo = ttk.Combobox(self, values=["camera", "mp4"])
+        self.src_combo.pack(pady=10)
+        self.camera_lbl = Label(self, text="Choose camera")
+        self.camera_lbl.pack(pady=5)
+        graph = FilterGraph()
+        self.devices = graph.get_input_devices()
+        self.camera_combo = ttk.Combobox(self, values=self.devices)
+        self.camera_combo.pack(pady=10)
+
+        self.multi_cls_btn = Button(self, text="Please choose mp4 file", width = 40, command=self.get_file_path)
+        self.multi_cls_btn.pack(pady=10)
+        self.file_path_lbl = Label(self, text="")
+        self.file_path_lbl.pack(pady=10)
+
+        self.btn_inf = Button(self, text="Real Time Prediction", width = 40, command=self.inf_rt)
+        self.btn_inf.pack(pady=10)
+
+        self.btn_back = Button(self, text="Back", width = 40, command=lambda: self.controller.show_frame("StartPage"))
+        self.btn_back.pack(pady=10)
+
+        self.update_text()
+
+    def get_file_path(self):
+        self.file_path = filedialog.askopenfilename(title="Select a video File", filetypes=(("mp4", "*.mp4"), ))
+
+    def update_text(self):
         if self.file_path != "":
-            self.file_path_lbl.config(text="file path = " + self.file_path)
-        if self.res != "":
-            self.lbl_inf.config(text="hasil prediksi = " + self.res)
-        self.after(100, self.update_single_text)
-    
-    def update_multi_text(self):
-        if self.multi_file_path != "":
-            self.multi_path_lbl.config(text="file path = " + self.multi_file_path)
-        self.after(100, self.update_multi_text)
+            self.file_path_lbl.config(text="File path = " + self.file_path)
+        self.after(100, self.update_text)
 
     def inf_rt(self):
-        self.withdraw()
         src = self.src_combo.get()
         cam = self.camera_combo.get()
         if src == "camera":
@@ -132,16 +157,16 @@ class app(tk.Tk):
             src_video = index
             flip = True
         elif src == "mp4":
-            if self.multi_file_path == "":
+            if self.file_path == "":
                 messagebox.showinfo("Error", "Select file mp4 first")
                 return
-            src_video = self.multi_file_path
+            src_video = self.file_path
             flip=False
         else:
            messagebox.showinfo("Error", "Select source first") 
+           return
         pose_detection.detect_real_time(src_video, self.model, flip=flip)
-        self.deiconify()
 
 if __name__ == "__main__":
-    window = app()
+    window = App()
     window.mainloop()
